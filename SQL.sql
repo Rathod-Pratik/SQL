11a166bff44a874b0aeaccea30e3a6cae28475ab
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-08-29 18:54:24 +0530

    Combine into one file

--- SQL.sql
+++ SQL.sql
@@ -1,8 +1,35 @@
+
+CREATE DATABASE myDB;
+
+CREATE DATABASE if not exists myDB;
+
+DROP DATABASE  mydb;
+
+DROP DATABASE if exists myDB;
+
+SHOW databases;
+
+
+use mydb;
+
+create table College(id int primary key,Name varchar(50),Course varchar(50));
+
+drop table College;
+
+INSERT into College
+(id,Name,Course)
+ value(1,"Rathod Pratik","BCA"),
+ (2,"Mohan dash","BCOM"),
+ (3,"Rohan dusara","BBA");
+
+select * from college;
+
+SHOW tables;
+
+
 create database constraints;
 use constraints;
 
-
---not insert any null value
 create table fees(
 id int not null,
 name varchar(50)
@@ -10,8 +37,6 @@ name varchar(50)
 insert into fees value(1,"Rathod");
 select * from fees;
 
-
---every value should be unique
 create table student(
 course varchar(5) unique,
 name varchar(50)
@@ -19,8 +44,6 @@ name varchar(50)
 insert into student value(1,"Rathod"),(2,"Dusara");
 select * from student;
 
-
---no duplicate value
 create table college(
 Enrollment_no int primary key,
 name varchar(50)
@@ -28,8 +51,6 @@ name varchar(50)
 insert into college value(1,"Rohan"),(2,"mohan");
 select * from college;
 
-
---default value
 create table employee(
 Id int ,
 salary int default 5000
@@ -37,7 +58,6 @@ salary int default 5000
 insert into employee (Id) value(15);
 select * from employee;
 
---add some condition
 create table shop(
 income int,
 invest int,
